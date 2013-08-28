8b1fd0169b4f7c435ee78047a118c854f3c9e23b
otmosina <otmosina@otmosina-N45SF.(none)>
2013-08-28 18:22:15 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppPull::Application.config.secret_token = 'cf85289ea7abf6b3e6df35ea091ff1133e99757fe0a24bfa16903c7eee1ae598115c97e31acadbfeb342e382181e84bfa129cdcca3404950fa0df1438eab686a'
